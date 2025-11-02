f190890ba3a26df3673400fb768b0e418a9210da
gujuluva-pixel <gujuluva.umich.edu>
2025-11-02 17:51:57 -0500

    added elementary nav bar, wip

--- analytics/views.py
+++ analytics/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def signup(request):
+    return render(request, 'registration/signup.html')
 
 def index(request):
     return render(request, 'analytics/index.html')
