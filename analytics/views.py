abb45a4c4d629d3775b76ac611129a22789fbedf
gujuluva-pixel <gujuluva.umich.edu>
2025-11-02 20:27:28 -0500

    edited index and deleted accounts

--- analytics/views.py
+++ analytics/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
 
 # Create your views here.
-
-def signup(request):
-    return render(request, 'registration/signup.html')
+#def signup(request):
+    #return render(request, 'registration/signup.html')
 
 def index(request):
     return render(request, 'analytics/index.html')
